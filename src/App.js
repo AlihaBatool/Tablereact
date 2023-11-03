ab1e83b5fe0c6ceef11b9dcb829c54cf147351ee
Aliha Batool <alihabatool2003@gmail.com>
2023-11-03 15:33:09 +0500

    table react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Table from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Table with Entries</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,34 @@
+// src/Table.js
+import React from 'react';
+
+const Table = () => {
+  // Sample data for the table
+  const data = [
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 3, name: 'Bob Johnson', email: 'bob@example.com' },
+  ];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((entry) => (
+          <tr key={entry.id}>
+            <td>{entry.id}</td>
+            <td>{entry.name}</td>
+            <td>{entry.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
